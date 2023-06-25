0020fcc97e5bf69fa83db0869986d7aef3f54198
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2023-06-25 22:21:42 +0200

    Prepared constant success and error messages

---
+++ server/src/constants/errorMessages.ts
@@ -0,0 +1,6 @@
+export const SERVER_ERROR = '500_1: Server error.';
+export const INVALID_VOTE = '400_1: Invalid vote.';
+export const RESOURCE_DOES_NOT_EXIST = '404_1: Resource does not exist.';
+export const STREAMER_DOES_NOT_EXIST = '404_2: Streamer does not exist.';
+export const UNPROCESSABLE_ENTITY = '422_1: Wrong data has been sent.';
+export const TOO_MANY_REQUESTS = '429_1: Too many requests.';
---
+++ server/src/constants/successMessages.ts
@@ -0,0 +1,2 @@
+export const VOTE_RECORDED = '200_1: Vote has been recorded.';
+export const NEW_STREAMER_ADDED = '201_1: New streamer added.';
