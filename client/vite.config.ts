e511b7e97791f9115230f501fe372dbdd19c9a1c
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2023-06-30 00:00:20 +0200

    Initialized client app

---
+++ client/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from '@rollup/plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [{ ...eslint({ include: 'src/**/*.+(js|jsx|ts|tsx)' }), enforce: 'pre' }, react()],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  },
+});
