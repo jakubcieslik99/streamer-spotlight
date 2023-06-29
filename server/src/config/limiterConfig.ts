118318f3a565df4a3b73c39664dbadd77bc762d1
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2023-06-29 19:52:17 +0200

    Updated server limiter

--- server/src/config/limiterConfig.ts
+++ server/src/config/limiterConfig.ts
@@ -2,15 +2,15 @@ import createError from 'http-errors';
 import { TOO_MANY_REQUESTS } from '../constants/errorMessages';
 
 export const rateLimiter = {
-  windowMs: 60 * 1000,
-  max: 15,
+  windowMs: 30 * 1000,
+  max: 30,
   handler: () => {
     throw createError(429, TOO_MANY_REQUESTS);
   },
 };
 
 export const speedLimiter = {
-  windowMs: 60 * 1000,
-  delayAfter: 5,
+  windowMs: 30 * 1000,
+  delayAfter: 25,
   delayMs: 200,
 };
