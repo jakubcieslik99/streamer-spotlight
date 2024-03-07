e23a6a0fff2f103fcdf34dc2d391fbb4eb64a48b
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2024-03-08 00:55:50 +0100

    Updated client dependencies, tsconfig, prettierrc & eslintrc files

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2020: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: '2020',
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+  },
+  root: true,
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from '@rollup/plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [{ ...eslint({ include: 'src/**/*.+(js|jsx|ts|tsx)' }), enforce: 'pre' }, react()],
+  plugins: [react()],
   server: {
     host: true,
     port: 3000,
